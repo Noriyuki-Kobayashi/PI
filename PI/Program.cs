801cd69c612900e550ae1a415ae24ba0b810ca73
Noriyuki-Kobayashi <noriyuki.kobayashi@outlook.com>
2021-05-08 11:25:56 +0900

    計算桁数を指定できるようにしました。 計算時間を表示するようにしました。

--- PI/Program.cs
+++ PI/Program.cs
@@ -1,4 +1,4 @@
-﻿// Visual C# 円周率を求めるプログラムです。 VIVOBOOKからPush
+﻿// Visual C# 円周率を求めるプログラムです。2021/5/8
 
 
 using System;
@@ -11,8 +11,28 @@ namespace PICalc
     {
         static void Main(string[] args)
         {
-            string pi = PICalculator.Calculate(10000);   // 計算する桁数を指定します。
-            Console.WriteLine(PICalculator.Format(pi));
+            string calc_count;
+            int int_calc_count;
+
+            Console.Write("円周率を何桁計算しますか？");
+            calc_count = Console.ReadLine();
+            int_calc_count = Int32.Parse(calc_count);
+            Console.Write("\n");
+
+            DateTime time_start;
+            DateTime time_end;
+
+            time_start = DateTime.Now;
+                        
+                string pi = PICalculator.Calculate(int_calc_count);
+                Console.WriteLine(PICalculator.Format(pi));
+
+            time_end = DateTime.Now;
+            var difference = time_end.Subtract(time_start);
+            Console.WriteLine($"\n計算時間は {difference.TotalSeconds.ToString("#,#.##")} secです。\n");
+            Console.ReadKey();
+
+
         }
     }
 
@@ -20,17 +40,17 @@ namespace PICalc
     static class PICalculator
     {
 
-        // 小数部figure桁までの円周率を求める
+        // 小数部figure桁までの円周率を計算します。
         public static string Calculate(int figure)
         {
-            // マチンの公式により、円周率を求める
+            // マチンの公式
             BigInteger a = ArcCot(5, figure) * 4;
             BigInteger b = ArcCot(239, figure);
             BigInteger pi = (a - b) * 4;
             return pi.ToString().Substring(0, figure + 1);   // +1 は、整数部 表示用
         }
 
-        // Calculateで求めた文字列を整形する。
+        // Calculateで求めた文字列を整形します。
         public static string Format(string s)
         {
             StringBuilder sb = new StringBuilder();
@@ -66,7 +86,7 @@ namespace PICalc
                     at = at + a;
                 else
                     at = at - a;
-                sign = sign * -1;  // signをトグルさせる
+                sign = sign * -1;  // signをトグルさせます。
             }
             return at;
         }
